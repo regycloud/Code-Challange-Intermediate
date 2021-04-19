5cb4a68c2fe069367a5ced6b3bb106926a0c8a3e
regycloud <regy888@gmail.com>
2021-04-19 08:25:04 +0700

    greetAliens is program to meet aliens by using function and greet them by using console.

---
+++ greetAliens.js
@@ -0,0 +1,21 @@
+// Write your code here:
+const greetAliens = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      console.log(`Oh powerful ${array[i]}, we humans offer our unconditional surrender!`);
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  // When you're ready to test your code, uncomment the below and run:
+  
+  const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+  
+  greetAliens(aliens);
+  
+  
